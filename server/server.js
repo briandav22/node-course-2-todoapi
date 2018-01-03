4e5fc7b7b85ccd7429a75a729451b96f6bfb8760
briandav22 <briandav22@gmail.com>
2018-01-03 10:19:09 -0500

    added get for todos

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ app.post('/todos', (req,res) => {
 
 });
 
+app.get('/todos', (req,res)=>{
+	Todo.find().then((results)=>{
+		res.send({results});
+	}, (e)=> {
+		res.status(400).send(e);
+	});
+
+});
+
 app.listen(3000, () => {
 	console.log('started on port 3000')
 })
